05eaede6478ba56b1211a250c0cf9e23f2e3bc28
Anil Madhavapeddy <anil@recoil.org>
2009-10-03 13:13:48 +0100

    test case for objects as well for type-conv extension

--- json-static/check_tc.ml
+++ json-static/check_tc.ml
@@ -1,4 +1,4 @@
-(* test file for the type-conv version *)
+(* test file for the type-conv version, compile with "make jtc-check_tc" *)
 
 type x = [ `One | `Two ]
 and y = Three of int | Four of (char * int32 * int64)
@@ -11,6 +11,11 @@ and t = {
   vat: y; 
 } with json
 
+type o = <
+  foo: string;
+  bar: int
+> with json
+
 let _ = 
   let h = Hashtbl.create 1 in 
   Hashtbl.add h "a" 1L;
@@ -19,4 +24,10 @@ let _ =
   let json_string = Json_io.string_of_json (json_of_t t) in
   let json_t = t_of_json (Json_io.json_of_string json_string) in
   assert(json_t = t);
+  prerr_endline json_string;
+  let o = object method foo="foo" method bar=1 end in
+  let json_string = Json_io.string_of_json (json_of_o o) in
+  let json_o = o_of_json (Json_io.json_of_string json_string) in
+  assert (json_o#foo = o#foo);
+  assert (json_o#bar = o#bar);
   prerr_endline json_string
